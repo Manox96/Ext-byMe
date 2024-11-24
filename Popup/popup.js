0f8281f36d54bfa9d6f94bdf8f1f017441f1143c
@Manox96 <aymanemanox15@gmail.com>
2024-11-24 16:14:59 +0100

    update

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,23 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let URL_input = document.getElementById('urlInput');
-    let URL_button = document.getElementById('submitBtn');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const toggleCheckbox = document.getElementById('checkBtn');
 
-    console.log('🔥🔥🔥 Script loaded 🔥🔥🔥');
+//     if (toggleCheckbox) {
+//         const extensionEnabled = localStorage.getItem('extensionEnabled') === 'true';
+//         toggleCheckbox.checked = extensionEnabled;
 
-    if (URL_button && URL_input) {
-        console.log(URL_button);
-        console.log(URL_input);
+//         toggleCheckbox.addEventListener('change', () => {
+//             const isEnabled = toggleCheckbox.checked;
+//             localStorage.setItem('extensionEnabled', isEnabled);
+//             console.log(`Extension ${isEnabled ? 'activée' : 'désactivée'}`);
 
-        URL_button.addEventListener('click', () => {
-            let msj = {
-                url: URL_input.value,
-                from: 'popup.js'
-            };
-            chrome.runtime.sendMessage(msj);
-            console.log('🔥🔥🔥 Message sent 🔥🔥🔥');
-            URL_button.value = "";
-        });
-    } else {
-        console.error('❌ URL_input or URL_button not found');
-    }
-});
+//             // Envoyer un message au script de fond pour démarrer/arrêter la logique
+//             chrome.runtime.sendMessage({
+//                 action: isEnabled ? 'start' : 'pause'
+//             });
+//         });
+//     }
+// });
+
+
+console.log('Popup script loaded');
